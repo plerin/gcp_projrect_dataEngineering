fc499666a79b6932195827eecaa34cbc0c109983
KiHoon <plerin152@gmail.com>
2021-07-24 20:54:04 +0900

    Update authorize_cloudshell.sh

--- 03_sqlstudio/authorize_cloudshell.sh
+++ 03_sqlstudio/authorize_cloudshell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcloud sql instances patch flights2 \
-	--authorized-networks $(wget -qO - http://ipecho.net/plain)/32
+	--authorized-networks $(wget -qO - http://ipecho.net/plain)/32 # ipecho.net에 요청을 보내 요청자의 IP주소를 반환받는다.(공인IP주소를 알아내는 방법이다.)
