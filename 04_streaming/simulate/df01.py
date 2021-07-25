45cc40ff9fdd191d36bbefd2662be2086070e642
KiHoon <plerin152@gmail.com>
2021-07-25 16:39:12 +0900

    Update df01.py

--- 04_streaming/simulate/df01.py
+++ 04_streaming/simulate/df01.py
@@ -18,17 +18,17 @@ import apache_beam as beam
 import csv
 
 if __name__ == '__main__':
-   with beam.Pipeline('DirectRunner') as pipeline:
+   with beam.Pipeline('DirectRunner') as pipeline: # 'DirectRunner' -> local에서 실행하겠다.
 
       airports = (pipeline
-         | beam.io.ReadFromText('airports.csv.gz')
+         | beam.io.ReadFromText('airports.csv.gz') # 이름 파일 읽어
          | beam.Map(lambda line: next(csv.reader([line])))
          | beam.Map(lambda fields: (fields[0], (fields[21], fields[26])))
       )
 
       (airports 
          | beam.Map(lambda airport_data: '{},{}'.format(airport_data[0], ','.join(airport_data[1])) )
-         | beam.io.WriteToText('extracted_airports')
+         | beam.io.WriteToText('extracted_airports')  # 이름 파일 로드
       )
 
       pipeline.run()
