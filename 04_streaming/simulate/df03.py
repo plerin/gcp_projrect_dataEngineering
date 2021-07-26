99b2debdd147d70d3d4554dc8a59f5732184a940
KiHoon <plerin152@gmail.com>
2021-07-26 16:16:46 +0900

    Update df03.py
    add comments with df03.py

--- 04_streaming/simulate/df03.py
+++ 04_streaming/simulate/df03.py
@@ -28,14 +28,14 @@ def addtimezone(lat, lon):
 
 def as_utc(date, hhmm, tzone):
    try:
-      if len(hhmm) > 0 and tzone is not None:
+      if len(hhmm) > 0 and tzone is not None:   # 데이터가 올바른지 확인
          import datetime, pytz
-         loc_tz = pytz.timezone(tzone)
-         loc_dt = loc_tz.localize(datetime.datetime.strptime(date,'%Y-%m-%d'), is_dst=False)
+         loc_tz = pytz.timezone(tzone) # 입력한 tzone의 timezone을 갖고와 
+         loc_dt = loc_tz.localize(datetime.datetime.strptime(date,'%Y-%m-%d'), is_dst=False) # 입력 데이터 timezone local에 맞도록 dataformat('%Y-%m-%d') 맞춰
          # can't just parse hhmm because the data contains 2400 and the like ...
-         loc_dt += datetime.timedelta(hours=int(hhmm[:2]), minutes=int(hhmm[2:]))
-         utc_dt = loc_dt.astimezone(pytz.utc)
-         return utc_dt.strftime('%Y-%m-%d %H:%M:%S')
+         loc_dt += datetime.timedelta(hours=int(hhmm[:2]), minutes=int(hhmm[2:])) # 시간은 입력된 hhmm 형식 데이터를 통해 hours/minutes로 나눠 입력
+         utc_dt = loc_dt.astimezone(pytz.utc)   # loc_dt 데이터를 'utc' timezone올 변경
+         return utc_dt.strftime('%Y-%m-%d %H:%M:%S') # 지정 포맷('%Y-%m-%d %H:%M:%S')으로 반환
       else:
          return '' # empty string corresponds to canceled flights
    except ValueError as e:
